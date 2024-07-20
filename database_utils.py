8d6fe07270ea93c3dad05378f41aad5444dcf890
Lo Ho <leo12211996@yahoo.com>
2024-07-20 09:45:46 +0800

    Create database class, add class and 4 functions (connect, add_member, get_animal_info, update_historical_data), complete connect function next

---
+++ database_utils.py
@@ -0,0 +1,17 @@
+# database connection and operations
+class DatabaseManager:
+    @staticmethod
+    def connect():
+        pass
+
+    @staticmethod
+    def add_member(user_data):
+        pass
+
+    @staticmethod
+    def get_animal_info(breed):
+        pass
+
+    @staticmethod
+    def update_historical_data(user, animal, feedback=None):
+        pass
