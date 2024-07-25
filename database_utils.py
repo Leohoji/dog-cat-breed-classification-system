30c4a8865d71bd971a645b40a7684dbed70beb1b
Lo Ho <leo12211996@yahoo.com>
2024-07-25 23:53:17 +0800

    Add function of DatabaseManager.connect, add connection and cursor, next time will create function named disconnect

--- database_utils.py
+++ database_utils.py
@@ -1,8 +1,15 @@
+import mysql.connector
+from mysql_password import PASSWORD
+
 # database connection and operations
 class DatabaseManager:
     @staticmethod
-    def connect():
-        pass
+    def connect(self):
+        self.connection = mysql.connector.connect(host='localhost',
+                                                  port='3306',
+                                                  user='root',
+                                                  password=PASSWORD)
+        self.cursor = self.connection.cursor()
 
     @staticmethod
     def add_member(user_data):
