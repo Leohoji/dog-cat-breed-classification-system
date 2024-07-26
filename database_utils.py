84defe3248dd473b6b8a578fe01224c55c0840a1
Lo Ho <leo12211996@yahoo.com>
2024-07-26 17:33:01 +0800

    Delete decorator, delete @staticmethod decorator for every function in DatabaseManager class, next time will modify login_verify function.

--- database_utils.py
+++ database_utils.py
@@ -3,7 +3,6 @@ from mysql_password import HOST, PORT, USER, PASSWORD
 
 # database connection and operations
 class DatabaseManager:
-    @staticmethod
     def connect(self):
         """
         Connect local MySQL database.
@@ -14,7 +13,6 @@ class DatabaseManager:
                                                   password=PASSWORD)
         self.cursor = self.connection.cursor()
     
-    @staticmethod
     def disconnect(self):
         """
         Disconnect local MySQL database.
@@ -22,22 +20,27 @@ class DatabaseManager:
         self.cursor.close()
         self.connection.close()
 
-    @staticmethod
     def login_verify(self, user_data:dict):
+        """
+        Verify user data from login interface.
+
+        Args:
+            user_data: Dictionary of user's information.
+        Returns:
+            String of 'user not exists', 'wrong info', or 'yes'.
+        """
         pass
 
-    @staticmethod
     def add_member(user_data):
         pass
 
-    @staticmethod
     def get_animal_info(breed):
         pass
 
-    @staticmethod
     def update_historical_data(user, animal, feedback=None):
         pass
 
 if __name__ == '__main__':
-    DatabaseManager.connect()
+    mysql_manager = DatabaseManager()
+    mysql_manager.connect()
     print('ok')
