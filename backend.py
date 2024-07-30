7d240c7dcefe2e0624c0fb730a4cea6aaabb29fb
Lo Ho <leo12211996@yahoo.com>
2024-07-30 17:49:42 +0800

    modify function, modify login_verify function in Verification class, next time will modify function sign_up_verify in Verification class

--- backend.py
+++ backend.py
@@ -1,5 +1,7 @@
 from model import DatabaseManager
 
+mysql_connector = DatabaseManager()
+
 class Verification:
     def login_verify(self, user_data:dict) -> str:
         """
@@ -10,7 +12,20 @@ class Verification:
         Returns:
             String of 'user not exists', 'wrong password', or 'yes'.
         """
+        member_info = mysql_connector.get_member_info(user_data)
 
-        pass
+        # check information
+        if not member_info:
+            result = 'user not exists'
+        else:
+            _, UserName, UserPassword, _ = member_info
+            result = (UserName, UserPassword)
+            print(f"output: {result}")
+            if member_info != UserPassword:
+                result = 'wrong password' # password is incorrect
+            else:
+                result = 'yes'
+
+        return result
     def sign_up_verify(self):
         pass
