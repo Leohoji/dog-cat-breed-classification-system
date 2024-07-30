c7aba715aea7e5648f24fde5aa09970973f5085f
Lo Ho <leo12211996@yahoo.com>
2024-07-30 17:33:01 +0800

    Rename file, rename view.py to backend..py

---
+++ backend.py
@@ -0,0 +1,16 @@
+from database_utils import DatabaseManager
+
+class Verification:
+    def login_verify(self, user_data:dict) -> str:
+        """
+        Verify user data from login interface.
+
+        Args:
+            user_data: Dictionary of user's information.
+        Returns:
+            String of 'user not exists', 'wrong password', or 'yes'.
+        """
+
+        pass
+    def sign_up_verify(self):
+        pass
--- view.py
+++
@@ -1,7 +0,0 @@
-
-
-class Verification:
-    def login_verify(self):
-        pass
-    def sign_up_verify(self):
-        pass
