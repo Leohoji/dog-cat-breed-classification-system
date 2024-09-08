a026590823772cbb50d386f044170c0bf1bc3abb
Lo Ho <leo12211996@yahoo.com>
2024-09-08 23:37:24 +0800

    create Classification class, this class is for model images preprocessing and model prediction

--- backend.py
+++ backend.py
@@ -1,5 +1,15 @@
+import cv2
+import json
+import base64
+import numpy as np
+from io import BytesIO
+from PIL import Image
+from tensorflow.keras.models import load_model
+from tensorflow.keras.applications.efficientnet import preprocess_input as EFNetPreProcessInput
+
 from model import DatabaseManager
 
+IMG_SIZE = (224, 224, 3)
 
 class Verification:
     def __init__(self):
@@ -51,12 +61,58 @@ class Verification:
         else: response = 'fail' 
 
         return response
-    
-    def collect_historical_data(self):
+
+class Classification:
+    def __init__(self, json_payload):
+        self.json_payload = json_payload
+
+        # Parse the JSON payload
+        self.data = json.loads(self.json_payload)
+
+        # Extract base64 image string and species 
+        self.species, self.base64_image_string = self.data['species'], self.data['image']
+
+        # Load real classes and classifier
+        self.real_classes = np.load('cats_classes.npy') \
+                            if self.species == 'cats' \
+                            else np.load('dogs_classes.npy')
+        self.classifier_loaded = load_model('./cats_classifier.h5') \
+                                 if self.species == 'cats' \
+                                 else load_model('./dogs_classifier.h5')
+
+        # Database manager object
+        self.mysql_connector = DatabaseManager() 
+
+    def decode_base64_image(self):
+        """Decode the base64 string and return the image as a PIL object"""
+        # Decode the base64 string back to bytes
+        img_data = base64.b64decode(self.base64_image_string)
+        
+        # Use BytesIO to convert the decoded bytes to an image
+        img = Image.open(BytesIO(img_data))
+        
+        # Convert the PIL image to a NumPy array
+        self.img_array = np.array(img)
+        
+        return self.img_array
+
+    def Model_Predict(self):
+        # Image preprocessing
+        image_rgb = self.decode_base64_image()
+        image_rgb = image_rgb.astype(np.float32)
+        resized_image = cv2.resize(image_rgb, IMG_SIZE) # resize image to (224, 224, 3)
+        processed_image = EFNetPreProcessInput(resized_image)  # preprocess image
+        image_for_pred = np.expand_dims(processed_image, axis=0) # add a batch dim
+        
+        # Model prediction
+        model_pred = self.classifier_loaded.predict(image_for_pred) # predict image
+        pred_index = np.argmax(np.squeeze(model_pred)) # get the highest class index
+        final_class = self.real_classes[pred_index] # final image class
+        
+        return final_class
+
+    def send_results(self):
         pass
 
     def save_to_historical_data(self):
-        pass
-
-    def send_results(self):
         pass
