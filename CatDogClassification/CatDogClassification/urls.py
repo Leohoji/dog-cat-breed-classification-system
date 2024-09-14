3ccd3a46c28084e738d1a262830cdf2d3202ac72
Lo Ho <leo12211996@yahoo.com>
2024-09-15 01:28:53 +0800

    Create login page

--- CatDogClassification/CatDogClassification/urls.py
+++ CatDogClassification/CatDogClassification/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from classification.views import show_login_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', show_login_page),
 ]
