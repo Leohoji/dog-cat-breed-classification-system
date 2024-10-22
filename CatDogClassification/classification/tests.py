62b86a6f120e6108d6a392fefed8ff6e94491f0b
Lo Ho <leo12211996@yahoo.com>
2024-10-22 20:53:24 +0800

    Modify test file and change the "show historical data" image

--- CatDogClassification/classification/tests.py
+++ CatDogClassification/classification/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+from mysql_manager import DatabaseManager
+
+# Test the database manager class
+class TestDatabaseManager(TestCase):
+    
+    @classmethod
+    def setUpTestData(cls):
+        """Set up non-modified objects used by all test methods"""
+        pass
